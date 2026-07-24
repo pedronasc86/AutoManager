63c56ed43fe2814c16d6963c6a9883dd440c7b1e
Diogo <jarraisatec@gmail.com>
2026-07-24 22:13:21 +0100

    AutoMapper e endpoints RF5, RF6, RF7

---
+++ AutoManager/PartsCatalog.API/Mappings/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using PartsCatalog.API.DTOs;
+using PartsCatalog.API.Models;
+
+namespace PartsCatalog.API.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Mapeia da Entidade Peca para o DTO de Resposta
+            CreateMap<Peca, PecaResponse>();
+
+            // Mapeia do DTO de Criação para a Entidade Peca
+            CreateMap<CriarPecaRequest, Peca>();
+
+            // Mapeia do DTO de Atualização para a Entidade Peca
+            CreateMap<AtualizarPecaRequest, Peca>();
+        }
+    }
+}
